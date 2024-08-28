96a7d77c99680ebed5f151f46aaafb06e4b8ada2
PalHair <palad.ru@mail.ru>
2024-08-28 16:44:57 +0400

    add sdk

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
+import { Command } from '@pipedrive/app-extensions-sdk';
 
 // SDK detects identifier from URL and uses default custom UI size
 const sdk = await new AppExtensionsSDK().initialize();
 
-
+await sdk.execute(Command.SHOW_SNACKBAR, {
+  message: 'Action completed',
+  link: {
+    url: 'https://app.pipedrive.com',
+    label: 'View',
+  },
+});
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
