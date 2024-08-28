4bc8b85c1fda4e33d4f997cb5e124db6e84939fa
PalHair <palad.ru@mail.ru>
2024-08-28 16:51:04 +0400

    add async func

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,19 @@ import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
 import { Command } from '@pipedrive/app-extensions-sdk';
 
 // SDK detects identifier from URL and uses default custom UI size
-const sdk = await new AppExtensionsSDK().initialize();
+const init = async () => {
 
-await sdk.execute(Command.SHOW_SNACKBAR, {
-  message: 'Action completed',
-  link: {
-    url: 'https://app.pipedrive.com',
-    label: 'View',
-  },
-});
-
+  const sdk = await new AppExtensionsSDK().initialize();
+  
+  await sdk.execute(Command.SHOW_SNACKBAR, {
+    message: 'Action completed',
+    link: {
+      url: 'https://app.pipedrive.com',
+      label: 'View',
+    },
+  });
+}
+init()
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <button>Test</button>
